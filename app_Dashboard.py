44761de5917f54463ce2567b801836d481bbf1de
SAHEL Bouchra <93081417+SAHELBouchra@users.noreply.github.com>
2026-04-21 14:25:14 +0100

    Increase dashboard height from 340px to 400px

--- app_Dashboard.py
+++ app_Dashboard.py
@@ -37,7 +37,7 @@ st.markdown("""
         padding: 22px;
         border-radius: 16px;
         border: 1px solid;
-        height: 340px !important;
+        height: 400px !important;
         display: flex;
         flex-direction: column;
     }
